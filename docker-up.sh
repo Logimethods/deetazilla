fe705b8cbda9762797c20599716253ce61ccd8ea
Laurent Magnin <laurent.magnin@logimethods.com>
2017-09-12 14:08:09 -0400

    [ci skip] OPTIONAL properties_path

--- docker-up.sh
+++ docker-up.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 # https://github.com/docker/compose/issues/3435#issuecomment-232353235
 
+while getopts ":p:" opt; do
+  case $opt in
+    p) properties_path="$OPTARG"
+    # echo "postfix: $postfix"
+    ((shift_nb+=2))
+    ;;
+    \?) echo "Invalid option $OPTARG"
+    ((shift_nb+=1))
+    ;;
+  esac
+done
+
 set -a
 location="$1"
 cluster_mode="$2"
@@ -20,13 +32,25 @@ include () {
 }
 
 include "properties/configuration.properties"
+include "${properties_path}/configuration.properties"
+
 include "properties/configuration-application.properties"
-include "properties/configuration-location-${location}.properties"
+include "${properties_path}/configuration-application.properties"
+
 include "properties/configuration-location-${location}-debug.properties"
+include "${properties_path}/configuration-location-${location}-debug.properties"
+
 include "properties/configuration-mode-${cluster_mode}.properties"
+include "${properties_path}/configuration-mode-${cluster_mode}.properties"
+
 include "properties/configuration-mode-${cluster_mode}-debug.properties"
+include "${properties_path}/configuration-mode-${cluster_mode}-debug.properties"
+
 include "properties/configuration-telegraf.properties"
+include "${properties_path}/configuration-telegraf.properties"
+
 include "properties/configuration-telegraf-debug.properties"
+include "${properties_path}/configuration-telegraf-debug.properties"
 set +a
 
 echo "DOCKER_COMPOSE_FILE: ${DOCKER_COMPOSE_FILE}"
