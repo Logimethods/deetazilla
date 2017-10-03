9f1d51f0a271cc071725170ea95d45433d304031
Laurent Magnin <laurent.magnin@logimethods.com>
2017-10-03 12:29:03 -0400

    zeppelin="zeppelin $root"

--- dz_compose/services_hierarchy-main.sh
+++ dz_compose/services_hierarchy-main.sh
@@ -4,7 +4,7 @@ metrics="metrics $root"
 spark="spark $root"
 cassandra="cassandra $root"
 hadoop="hadoop $root"
-zeppelin="zeppelin"
+zeppelin="zeppelin $root"
 
 root_metrics="root_metrics $root $metrics"
 
