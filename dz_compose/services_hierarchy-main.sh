e185fd55aeca8a413dc542beed1a851f9956f790
Laurent Magnin <laurent.magnin@logimethods.com>
2017-10-10 13:19:52 -0400

    zeppelin***_***external_volumes

--- dz_compose/services_hierarchy-main.sh
+++ dz_compose/services_hierarchy-main.sh
@@ -5,7 +5,7 @@ spark="spark $root"
 cassandra="cassandra $root"
 hadoop="hadoop $root"
 zeppelin="zeppelin $root"
-zeppelin-external_volumes="zeppelin-external_volumes $zeppelin"
+zeppelin_external_volumes="zeppelin-external_volumes $zeppelin"
 
 root_metrics="root_metrics $root $metrics"
 
