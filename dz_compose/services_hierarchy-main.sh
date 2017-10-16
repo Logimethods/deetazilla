3336fb63aa47564d25a097d994ef34d1ab15db4f
Laurent Magnin <laurent.magnin@logimethods.com>
2017-10-16 15:11:20 -0400

    deploy MOVED TO -cluster.yml

--- dz_compose/services_hierarchy-main.sh
+++ dz_compose/services_hierarchy-main.sh
@@ -3,7 +3,7 @@ root="root root-${CLUSTER_MODE} root${SECRET_MODE}"
 metrics="metrics $root"
 spark="spark spark-${CLUSTER_MODE} $root"
 cassandra="cassandra cassandra-image cassandra-${CLUSTER_MODE} $root"
-hadoop="hadoop $root"
+hadoop="hadoop hadoop-${CLUSTER_MODE} $root"
 zeppelin="zeppelin $root"
 zeppelin-external_volumes="$zeppelin zeppelin-external_volumes $root"
 
