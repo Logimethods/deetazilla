78e131235f2b52e9a994122f5de2f6e733f87852
Laurent Magnin <laurent.magnin@logimethods.com>
2017-10-18 11:10:42 -0400

    Straming no more depending on Cassandra

--- scripts/compose_deploy.sh
+++ scripts/compose_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker pull logimethods/int_compose:1.0-dev
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "_no-secrets" inject_metrics > docker-compose-merge.yml
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "_no-secrets" inject_metrics streaming > docker-compose-merge.yml
 docker stack deploy -c docker-compose-merge.yml deetazilla
