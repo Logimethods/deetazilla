225f93abdebfdac7c5849c9c2c48160086ca3bc6
Laurent Magnin <laurent.magnin@logimethods.com>
2017-10-26 17:31:30 -0400

    else   ___intro=""

--- dz_templater/set_properties_to_templates.sh
+++ dz_templater/set_properties_to_templates.sh
@@ -8,9 +8,8 @@ while IFS= read -r -d '' filename; do
 echo "-------------- $filename --------------"
 if [[ ${filename} == */Dockerfile.tpl || ${filename} == *.sh.tpl ]]; then
   ___intro="### GENERATED FILE, please do not modify nor store into Git ###"
-fi
-if [[ ${filename} == *.xml.tpl ]]; then
-  ___intro="<!-- GENERATED FILE, please do not modify nor store into Git -->"
+else
+  ___intro=""
 fi
 eval "cat <<EOF
 ${___intro}
