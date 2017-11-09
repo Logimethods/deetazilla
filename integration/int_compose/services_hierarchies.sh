33300375312a8a06bbd6f51b29b110e2b4f8961b
Laurent Magnin <laurent.magnin@logimethods.com>
2017-11-09 11:34:51 -0500

    root_debug + "-${SECRET_MODE}"

--- integration/int_compose/services_hierarchies.sh
+++ integration/int_compose/services_hierarchies.sh
@@ -1,15 +1,15 @@
 source ./services_hierarchy-main.sh
 
-inject="inject inject${SECRET_MODE} inject-${CLUSTER_MODE} $metrics $root"
-inject_metrics="$inject $metrics"
+inject="inject inject-${SECRET_MODE} inject-${CLUSTER_MODE} $root"
+inject_metrics="$inject inject_metrics $metrics"
 
-cassandra_inject="cassandra_inject cassandra_inject-${CLUSTER_MODE} cassandra_inject${SECRET_MODE} $cassandra $root"
+cassandra_inject="cassandra_inject cassandra_inject-${CLUSTER_MODE} cassandra_inject-${SECRET_MODE} $cassandra $root"
 cassandra_inject_metrics="cassandra_inject_metrics $cassandra $metrics"
 
-streaming="streaming streaming${SECRET_MODE} $root $spark"
-streaming_metrics="streaming_metrics streaming_metrics${SECRET_MODE} $streaming $metrics"
+streaming="streaming streaming-${SECRET_MODE} $root $spark"
+streaming_metrics="streaming_metrics streaming_metrics-${SECRET_MODE} $streaming $metrics"
 
-monitoring="monitoring monitoring${SECRET_MODE} $root"
+monitoring="monitoring monitoring-${SECRET_MODE} $root"
 monitoring_metrics="monitoring_metrics $monitoring $metrics"
 
 integration_app="$inject $streaming $monitoring"
--- scripts/compose_deploy.sh
+++ scripts/compose_deploy.sh
@@ -3,5 +3,5 @@
 docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
 docker pull logimethods/int_compose:1.0-dev
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "_no-secrets" inject streaming monitoring > docker-compose-merge.yml
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "no_secrets" inject streaming monitoring > docker-compose-merge.yml
 docker stack deploy -c docker-compose-merge.yml deetazilla
