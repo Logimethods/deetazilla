725fe96716c6ac02ce3c3addf18bd5b0344c6339
Laurent Magnin <laurent.magnin@logimethods.com>
2017-11-09 10:54:34 -0500

    docker network create **--attachable**
    To be compatible with docker-compose.

--- scripts/compose_deploy.sh
+++ scripts/compose_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker network create --driver overlay deetazilla
+docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
 docker pull logimethods/int_compose:1.0-dev
 docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "_no-secrets" inject streaming monitoring > docker-compose-merge.yml
