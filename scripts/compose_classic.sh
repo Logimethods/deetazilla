07992dd13d69a6b15dc65e6490c9901266d916bf
Laurent Magnin <laurent.magnin@logimethods.com>
2017-12-06 11:14:02 -0500

    Deploy Scripts ADDED

--- scripts/compose_classic.sh
+++ scripts/compose_classic.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+docker pull logimethods/int_compose:1.0-dev
+docker pull logimethods/int_inject:1.0-dev
+docker pull logimethods/int_streaming:1.0-dev
+docker pull logimethods/int_cassandra:1.0-dev
+#docker pull logimethods/int_cassandra_inject:1.0-dev
+docker pull logimethods/int_monitor:1.0-dev
+
 docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
 docker pull logimethods/int_compose:1.0-dev
 
 docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev \
   combine_services -p alt_properties -e "local" "single" "no_secrets" \
-  inject streaming monitor > docker-compose-merge.yml
+  inject streaming monitor cassandra > docker-compose-merge.yml
+#  inject streaming monitor > docker-compose-merge.yml
 
 docker-compose -f docker-compose-merge.yml up
 
-docker-compose -f docker-compose-merge.yml up
+docker-compose -f docker-compose-merge.yml down
---
+++ scripts/compose_deploy-full.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker stack rm deetazilla
+docker pull logimethods/int_compose:1.0-dev
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "no_secrets" inject_metrics streaming > docker-compose-merge.yml
+docker stack deploy -c docker-compose-merge.yml deetazilla
