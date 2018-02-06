00671d7092a243a7f3eeb0b14644369c7c90e49a
Laurent Magnin <laurent.magnin@logimethods.com>
2018-02-06 15:01:27 -0500

    Version 1.5

--- scripts/compose_classic.sh
+++ scripts/compose_classic.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-docker pull logimethods/int_compose:1.0-dev
-docker pull logimethods/int_inject:1.0-dev
-docker pull logimethods/int_streaming:1.0-dev
-docker pull logimethods/int_cassandra:1.0-dev
-#docker pull logimethods/int_cassandra_inject:1.0-dev
-docker pull logimethods/int_monitor:1.0-dev
+docker pull logimethods/int_compose:1.5
+docker pull logimethods/int_inject:1.5
+docker pull logimethods/int_streaming:1.5
+docker pull logimethods/int_cassandra:1.5
+#docker pull logimethods/int_cassandra_inject:1.5
+docker pull logimethods/int_monitor:1.5
 
 docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
-docker pull logimethods/int_compose:1.0-dev
+docker pull logimethods/int_compose:1.5
 
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev \
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.5 \
   combine_services -p alt_properties -e "local" "single" "no_secrets" \
   inject streaming monitoring > docker-compose-merge.yml
 
--- scripts/compose_deploy.sh
+++ scripts/compose_deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker pull logimethods/int_cassandra_inject:1.0-dev
+docker pull logimethods/int_cassandra_inject:1.5-dev
 
 docker network create --attachable --driver overlay deetazilla
 docker stack rm deetazilla
-docker pull logimethods/int_compose:1.0-dev
-docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.0-dev combine_services -p alt_properties -e "local" "single" "no_secrets" \
+docker pull logimethods/int_compose:1.5-dev
+docker run --rm -v `pwd`/alt_properties:/templater/alt_properties logimethods/int_compose:1.5-dev combine_services -p alt_properties -e "local" "single" "no_secrets" \
   inject streaming cassandra cassandra_inject > docker-compose-merge.yml
 #  inject streaming monitor cassandra cassandra_inject > docker-compose-merge.yml
 docker stack deploy -c docker-compose-merge.yml deetazilla
